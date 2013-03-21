0e449d0a1cd8ff902216f20caeb7d881d766a8b0
Stephen Done <srdone@gmail.com>
2013-03-21 11:59:09 -0700

    Additional Changes

--- src/ui/application/Main.java
+++ src/ui/application/Main.java
@@ -6,8 +6,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		FileIO fileIO = new FileIO();
+		fileIO.runApp();
 	}
 
 }
